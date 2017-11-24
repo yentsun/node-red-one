9d3126242c635b89814a25ab73816c4528e8145a
Maxim Korinets <mkorinets@gmail.com>
2017-11-24 10:06:27 +0700

    Add Node-RED `settings.js`

---
+++ settings.js
@@ -0,0 +1,18 @@
+module.exports = {
+  uiPort: process.env.PORT || 1880,
+  mqttReconnectTime: 15000,
+  serialReconnectTime: 15000,
+  debugMaxLength: 1000,
+
+  functionGlobalContext: {
+    moment: require('moment')
+  },
+
+  logging: {
+    console: {
+      level: "info",
+      metrics: false,
+      audit: false
+    }
+  }
+};
